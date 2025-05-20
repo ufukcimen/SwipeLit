4081483c2768370c8b842cfe1ac8fda0d663c56e
damla-ucar7 <damla.ucar@sabanciuniv.edu>
2025-05-20 15:41:36 +0300

    Add files via upload

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.swipelit"
-
+    namespace = "com.example.pain"
     compileSdk = flutter.compileSdkVersion
-
-    // ✅ Force correct NDK version for plugin compatibility
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -23,8 +23,11 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.swipelit"
-        minSdk = flutter.minSdkVersion
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.pain"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdkVersion (23)
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -32,6 +35,8 @@ android {
 
     buildTypes {
         release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
---
+++ android/app/src/main/kotlin/com/example/pain/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pain
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
